5541573fd76e8758dacb77ff82596efa988b36b2
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-11-21 19:29:03 +0900

    feat: 댓글 UI 구현

---
+++ app/_components/post/CommentItem.tsx
@@ -0,0 +1,23 @@
+import profile from "@/public/images/profile.svg";
+import Image from "next/image";
+
+const CommentItem = ({ comment }: { comment: { name: string; content: string } }) => {
+  return (
+    <div className="comment-block">
+      <div className="profile-area">
+        <Image src={profile} alt="profile" />
+        <div>{comment.name}</div>
+      </div>
+
+      <div className="comment-content-area">
+        <div className="comment-content">{comment.content}</div>
+        <div className="comment-under">
+          <div className="comment-reply">Reply</div>
+          <div className="comment-time">a min ago</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentItem;
